88ac21c16954b559f54694140cc5d3accfc70812
Domingos de Jesus Alcantara <45907626+DomingosAlcantara@users.noreply.github.com>
2023-03-27 21:39:15 -0300

    Criando arquivos de testes

---
+++ Pongbolim/__tests__/campo_test.js
@@ -0,0 +1,16 @@
+const dimensoes = {
+    afastamento: {
+        x: 10,
+        y: 30,
+    },
+    altura: 450,
+    comprimento: 780,
+}
+
+describe("Testando a criação do campo", () => {
+    test("Testando a área do campo", () => {
+        const Campo = new Campo(dimensoes);
+
+        
+    })
+})
---
+++ Pongbolim/__tests__/sketch_test.js
@@ -0,0 +1,5 @@
+describe("Testes para Refatoração", () => {
+    test("Desenhando a área do campo", () => {
+        
+    })
+})
