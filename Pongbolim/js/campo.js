ae9970be41e92cd19fe892c265563f2eb2571ec4
Domingos de Jesus Alcantara <domingos_alcantara@yahoo.com.br>
2023-04-11 09:57:30 -0300

    Últimas alterações no arquivo Campo.js

--- Pongbolim/js/campo.js
+++ Pongbolim/js/campo.js
@@ -1,29 +1,11 @@
-const Campo = class {
-  #altura = 0;
-  #largura = 0;
-  constructor(dimensoesGerais) {
-    this.altura(dimensoesGerais.altura);
-  }
+const p5 = require("./p5/p5");
 
-  set altura(h) {
-    try {
-      this.#altura = h;
-    } catch {
-      this.#altura = 0;
-    }
-  }
-
-  set largura(w) {
-    try {
-      this.#largura = w;
-    } catch {
-      this.#largura = 0;
-    }
-  }
-
-  isValid(value) {
-    return Number.isInteger(value);
-  }
+const areaTotalDoCampo = ({ dimensoes }) => {
+  return p5.createCanvas(dimensoes.comprimento, dimensoes.altura);
 };
 
-module.exports = Campo();
+const desenharAreaUtil = ()
+
+module.exports = {
+  areaTotalDoCampo,
+};
